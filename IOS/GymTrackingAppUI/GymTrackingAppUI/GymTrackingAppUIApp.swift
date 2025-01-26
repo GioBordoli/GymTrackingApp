f8d9febc5bebc7f062632b24e1bef1b202e29a79
Giorgio Bordoli <giorgio.bordoli01@gmail.com>
2025-01-25 23:31:27 -0700

    Fix GymTrackingAppUI submodule issue

---
+++ IOS/GymTrackingAppUI/GymTrackingAppUI/GymTrackingAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackingAppUIApp.swift
+//  GymTrackingAppUI
+//
+//  Created by Giorgio Bordoli on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct GymTrackingAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS/GymTrackingAppUI/GymTrackingAppUITests/GymTrackingAppUITests.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackingAppUITests.swift
+//  GymTrackingAppUITests
+//
+//  Created by Giorgio Bordoli on 1/25/25.
+//
+
+import Testing
+@testable import GymTrackingAppUI
+
+struct GymTrackingAppUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
